5e732cb4fb3f647651510a015700fd56092bde0a
QShuhrat <93004364+QShuhrat@users.noreply.github.com>
2022-09-01 09:58:24 +0500

    fasdf

--- main.py
+++ main.py
@@ -3,31 +3,28 @@ import wikipedia as w
 import logging
 
 
-
 logging.basicConfig(level=logging.INFO)
 
 w.set_lang("uz")
 
-TOKEN = 'FOR TOKEN'
+TOKEN = 'TOKEN UCHUN'
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
-    await message.reply("""Wikipeida Botiga Xush Kelibsiz!
-                            Siz hohlagan narsangizni qidiridirishingiz mumkin.
-                            Qidirish uchun kerakli bo'lgan matnnit kiriting!!!""")
+    await message.reply("Wikipeida Botiga Xush Kelibsiz!\nSiz hohlagan narsangizni qidiridirishingiz mumkin.\nQidirish uchun kerakli bo'lgan matnnit kiriting!!!")
 
 @dp.message_handler(commands=["help"])
 async def cmd_help(message: types.Message):
-    await message.reply("Just send me a word or phrase and I'll search for it.")
+    await message.reply("Qidirilayotgan matnni kiriting!!!")
 
 @dp.message_handler()
 async def send_wiki(message: types.Message):
     try:
         await message.reply(w.summary(message.text))
     except:
-        await message.reply("I can't find anything for you :(")
+        await message.reply("Siz izlayotgan narsa topilmadi😅")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
